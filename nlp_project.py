e9136fca6ed0a9cf04fac2d9b1b5390563320a1f
Lenaghan, Terry <tlenaghan@gmail.com>
2024-12-11 22:10:14 -0500

    enlarging datasets

--- nlp_project.py
+++ nlp_project.py
@@ -13,7 +13,7 @@ print('numpy: {}'.format(np.__version__))
 
 # Load the datasets
 df_data_cb = pd.read_csv('./data/data_cb.csv')
-df_data_wos = pd.read_csv('./data/data_wos.csv')
+df_data_wos = pd.read_csv('./data/web_of_science_data.csv') #('./data/data_wos.csv')
 
 # Preview the Clickbait data
 print(f'\nNumber of headlines in Clickbait dataset: {len(df_data_cb)}')
@@ -30,9 +30,9 @@ print(df_data_cb[['headline', 'label']][x:x+5])
 print(f'\nNumber of Web of Science Articles: {len(df_data_wos)}')
 
 # Numerical label to domain mapping
-wos_label = {0:'CS', 1:'ECE', 4:'Civil', 5:'Medical'}
+wos_label = dict(zip(df_data_wos['Y1'], df_data_wos['Domain']))
 
-# Out labels as a table
+# Output labels as a table
 wos_label_df = pd.DataFrame(list(wos_label.items()),columns=['Label','Description'])
 print("\nKey:")
 print(wos_label_df)
@@ -41,16 +41,16 @@ print(wos_label_df)
 print("\nSample:")
 x=17
 sample_wos_data = {
-    'Label': df_data_wos['label'][x:x+5],
-    'Description': [wos_label[label] for label in df_data_wos['label'][x:x+5]],
-    'Article': df_data_wos['article'][x:x+5]
+    'Label': df_data_wos['Y1'][x:x+5],
+    'Description': [wos_label.get(label.strip(), label) for label in df_data_wos['Domain'][x:x+5]],
+    'Article': df_data_wos['Abstract'][x:x+5]
 }
 
 sample_wos_df = pd.DataFrame(sample_wos_data)
 print(sample_wos_df)
 
 print('\nWeb of Science dataset source format:')
-print(df_data_wos[['article', 'label']][x:x+5])
+print(df_data_wos[x:x+5])
 
 import split
 
@@ -61,8 +61,8 @@ df_train_wos, df_test_wos = split.split_data(df_data_wos)
 # separate the independent and dependent variables
 x_train_cb, y_train_cb = list(df_train_cb['headline']), list(df_train_cb['label'])
 x_test_cb, y_test_cb = list(df_test_cb['headline']), list(df_test_cb['label'])
-x_train_wos, y_train_wos = list(df_train_wos['article']), list(df_train_wos['label'])
-x_test_wos, y_test_wos = list(df_test_wos['article']), list(df_test_wos['label'])
+x_train_wos, y_train_wos = list(df_train_wos['Abstract']), list(df_train_wos['Domain'])
+x_test_wos, y_test_wos = list(df_test_wos['Abstract']), list(df_test_wos['Domain'])
 
 print(f'\nClickbait dataset training headline count: {len(x_train_cb)}')
 print(f'Clickbait dataset test headline count: {len(x_test_cb)}')
